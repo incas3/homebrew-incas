07736099f53cb15abe2d94995cd10c43447aa588
Dirkjan Krijnders <dirkjankrijnders@incas3.eu>
2013-04-22 11:59:02 +0200

    Point to github repo instead aubio's own git

--- aubio.rb
+++ aubio.rb
@@ -4,7 +4,8 @@ class Aubio < Formula
   homepage 'http://aubio.org/'
   url 'http://aubio.org/pub/aubio-0.3.2.tar.gz'
   sha1 '8ef7ccbf18a4fa6db712a9192acafc9c8d080978'
-  head 'git://git.aubio.org/git/aubio/'
+#  head 'git://git.aubio.org/git/aubio/'
+  head 'git@github.com:dirkjankrijnders/aubio.git'
 
   depends_on 'pkg-config' => :build
   depends_on 'fftw' => :recommended
@@ -14,16 +15,16 @@ class Aubio < Formula
   depends_on 'swig' => :optional
 
   def install
-    if build.head?
-      system "./waf configure"
-      system "./waf"
-      system "./waf install"
-    else
+#    if build.head?
+#      system "./waf configure"
+#      system "./waf"
+#      system "./waf install"
+#    else
       system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
       system "make"
       ENV.j1
       system "make install"
-    end
+#    end
   end
 end
