50bbf108bf770101ac2a3044395ebb1d3c95c4bf
Dirkjan Krijnders <dirkjankrijnders@incas3.eu>
2013-06-10 11:13:28 +0200

    Working recipe

--- ambdec.rb
+++ ambdec.rb
@@ -6,12 +6,21 @@ class Ambdec < Formula
   sha1 'e0a925c76d2a46bb5b2838e249ee7a86638b335a'
 
   depends_on 'clthreads'
-	depends_in 'clxclient'
+	depends_on 'clxclient'
 
   def install
-    system "make", "install" # if this fails, try separate make/make install steps
+		
+		inreplace 'source/Makefile', '/usr/local', "#{prefix}"
+		
+		system "cd source"
+		system "cd source; make"
+    system "cd source; make install" # if this fails, try separate make/make install steps
   end
-
+	
+  def patches
+    DATA
+  end
+	
   test do
     # `test do` will create, run in and delete a temporary directory.
     #
@@ -21,3 +30,27 @@ class Ambdec < Formula
     system "false"
   end
 end
+
+__END__
+diff --git a/source/Makefile b/source/Makefile
+index 36f7898..22af5d5 100644
+--- a/source/Makefile
++++ b/source/Makefile
+@@ -36,7 +36,7 @@ AMBDEC_O = ambdec.o styles.o mainwin.o confwin.o radbut.o filewin.o \
+            png2img.o meter.o
+ 
+ ambdec:	CPPFLAGS += -I/usr/X11R6/include `freetype-config --cflags`
+-ambdec:	LDLIBS += -lclxclient -lclthreads -ljack -lpng -lXft -lX11 -lrt
++ambdec:	LDLIBS += -lclxclient -lclthreads -ljack -lpng -lXft -lX11
+ ambdec:	LDFLAGS += -L/usr/X11R6/lib
+ ambdec:	$(AMBDEC_O)
+ 	g++ $(LDFLAGS) -o $@ $(AMBDEC_O) $(LDLIBS)
+@@ -48,7 +48,7 @@ $(AMBDEC_O):
+ 
+ AMBDEC_CLI_O = ambdec_cli.o jclient.o nffilt.o xover2.o decoder.o adconf.o sstring.o
+ 
+-ambdec_cli:	LDLIBS += -lclthreads -ljack -lrt
++ambdec_cli:	LDLIBS += -lclthreads -ljack
+ ambdec_cli:	$(AMBDEC_CLI_O)
+ 	g++ $(LDFLAGS) -o $@ $(AMBDEC_CLI_O) $(LDLIBS)
+ 
