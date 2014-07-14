53d2e5fbfd89869c056081d0350837798f789d8b
Erik Kallen <info@erikkallen.nl>
2014-07-14 10:57:52 +0200

    version 2

--- libreadout2.rb
+++ libreadout2.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Libreadout2 < Formula
   homepage 'http://redmine.incas3.nl/projects/scintillator-readout-chains/ensa/libreadout2'
-  head 'ssh://git@git.incas3.nl/scintillator-readout-chains/ensa/libreadout2.git', :using => :git
+  head 'ssh://git@git.incas3.nl/scintillator-readout-chains/ensa/libreadout2.git', :using => :git, :branch => "v4.0_250mhz"
 
   # depends_on 'cmake' => :build
 
